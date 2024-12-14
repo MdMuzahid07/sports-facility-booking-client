0735672faf65085a6b8020bb22b58f21100df663
MdMuzahid07 <mdmuzahid7396@gmail.com>
2024-12-14 19:55:05 +0600

    loading spinner, ui update

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,8 +1,12 @@
 
 const LoadingSpinner = () => {
     return (
-        <section className="h-screen w-full flex justify-center items-center bg-slate-200">
-            <h1 className="text-xl font-extrabold">Loading...</h1>
+        <section className="py-32 w-full flex justify-center flex-col items-center bg-transparent">
+            <div className="relative flex h-24 w-24">
+                <img className="w-24 h-24 absolute left-0 top-0 rounded-full" src="https://res.cloudinary.com/dymo0iyee/image/upload/v1725714475/playTime-pro_qrlpvx.png" alt="" />
+                <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-white opacity-75"></span>
+            </div>
+            <h1 className="text-xl font-extrabold mt-2">Loading...</h1>
         </section>
     )
 };
--- src/components/main/onSale/WinterSaleFacilities.tsx
+++ src/components/main/onSale/WinterSaleFacilities.tsx
@@ -1,10 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import LoadingSpinner from "@/components/LoadingSpinner";
 import { Button } from "@/components/ui/button";
 import { useGetAllFacilitiesQuery } from "@/redux/features/facilities/facilityApi";
 import { useNavigate } from "react-router-dom";
 
 const WinterSaleFacilities = () => {
-    const { data } = useGetAllFacilitiesQuery(undefined);
+    const { data, isLoading } = useGetAllFacilitiesQuery(undefined);
     const navigate = useNavigate();
 
     const filtered = data?.data?.filter((facility: any) =>
@@ -18,40 +19,43 @@ const WinterSaleFacilities = () => {
     };
 
 
-
     return (
         <section className="pt-28 pb-32 bg-[#2E2E2E] text-slate-200">
             <div className="max-w-7xl mx-auto px-4 xl:px-0">
                 <h2 className="text-3xl md:text-5xl font-extrabold mb-5 text-white">Winter Sale</h2>
                 <p className="text-xl md:text-2xl lg:text-3xl">Stay Healthy, Stay Active, Save Big!</p>
 
-                <section className="grid md:grid-cols-2 gap-8 mt-16">
-                    {
-                        filtered?.map((facility: any) => (
-                            <div
-                                className="relative bg-cover bg-center h-96 flex items-center justify-start"
-                                style={{
-                                    backgroundImage: `url(${facility?.image})`,
-                                }}
-                            >
-                                {/* Overlay */}
-                                <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+                {
+                    isLoading ? (<LoadingSpinner />) : (
+                        <section className="grid md:grid-cols-2 gap-8 mt-16">
+                            {
+                                filtered?.map((facility: any) => (
+                                    <div
+                                        className="relative bg-cover bg-center h-96 flex items-center justify-start"
+                                        style={{
+                                            backgroundImage: `url(${facility?.image})`,
+                                        }}
+                                    >
+                                        {/* Overlay */}
+                                        <div className="absolute inset-0 bg-black bg-opacity-50"></div>
 
-                                {/* Content */}
-                                <div className="relative z-10 px-6 md:px-12">
-                                    <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold text-white mb-4">
-                                        {facility?.name}
-                                    </h2>
-                                    <p className="text-gray-300 text-lg md:text-xl leading-relaxed mb-6">
-                                        {facility?.description}
-                                    </p>
-                                    <Button onClick={() => handleRedirectToDetails(facility?._id)} className="mt-6 rounded-none text-2xl md:text-3xl lg:text-4xl">View More</Button>
+                                        {/* Content */}
+                                        <div className="relative z-10 px-6 md:px-12">
+                                            <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold text-white mb-4">
+                                                {facility?.name}
+                                            </h2>
+                                            <p className="text-gray-300 text-lg md:text-xl leading-relaxed mb-6">
+                                                {facility?.description}
+                                            </p>
+                                            <Button onClick={() => handleRedirectToDetails(facility?._id)} className="mt-6 rounded-none text-2xl md:text-3xl lg:text-4xl">View More</Button>
 
-                                </div>
-                            </div>
-                        ))
-                    }
-                </section>
+                                        </div>
+                                    </div>
+                                ))
+                            }
+                        </section>
+                    )
+                }
             </div>
         </section>
     )
