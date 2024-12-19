b68827da5934f59be2f526f27f809076c47944b7
MdMuzahid07 <mdmuzahid7396@gmail.com>
2024-12-19 10:00:39 +0600

    User mybooking details page bug fixed, and UI/UX code refactored

--- src/pages/dashboard/MyBookingDetails.tsx
+++ src/pages/dashboard/MyBookingDetails.tsx
@@ -17,66 +17,81 @@ const MyBookingDetails = () => {
                 {/* Facility Information */}
                 <section className="mb-6">
                     <h3 className="text-2xl font-extrabold text-primary">Facility</h3>
-                    <section className="flex flex-col lg:flex-row mt-4 gap-6">
-                        <img
-                            className="w-full md:w-1/2 rounded-lg"
-                            src={facility?.image}
-                            alt={facility?.name}
-                        />
-                        <section className="md:w-2/3">
+                    <section className="grid md:grid-cols-2 mt-4 gap-6">
+                        <div>
+                            <img
+                                className="w-full max-h-[500px] rounded-lg"
+                                src={facility?.image?.[0]}
+                                alt={facility?.name}
+                            />
+                            <div className="flex lg:flex-row gap-8 mt-8">
+                                {/* User Information */}
+                                <div className="mb-6">
+                                    <h3 className="text-2xl font-extrabold text-primary">User</h3>
+                                    <div className="mt-4">
+                                        <p className="text-primary text-xl">
+                                            <strong>Name:</strong> {user?.name}
+                                        </p>
+                                        <p className="text-primary text-xl">
+                                            <strong>Email:</strong> {user?.email}
+                                        </p>
+                                        <p className="text-primary text-xl">
+                                            <strong>Phone:</strong> {user?.phone}
+                                        </p>
+                                        <p className="text-primary text-xl">
+                                            <strong>Address:</strong> {user?.address}
+                                        </p>
+                                    </div>
+                                </div>
+
+                                {/* Booking Information */}
+                                <div className="mb-6">
+                                    <h3 className="text-2xl font-extrabold text-primary">Booking Info</h3>
+                                    <div className="mt-4">
+                                        <p className="text-primary text-xl">
+                                            <strong>Date:</strong> {date ? date : "date"}
+                                        </p>
+                                        <p className="text-primary text-xl">
+                                            <strong>Time:</strong> {startTime ? startTime : "startTime"} - {endTime ? endTime : "endTime"}
+                                        </p>
+                                        <p className="text-primary text-xl">
+                                            <strong>Payable Amount:</strong> ${payableAmount ? payableAmount : "payableAmount"}
+                                        </p>
+                                        <p className={`text-lg font-semibold mt-4 ${isBooked === "canceled" ? "text-red-500" : "text-green-500"}`}>
+                                            <span className="text-primary font-bold"> Status:</span> {isBooked}
+                                        </p>
+                                        <p className="text-primary text-xl mt-2">
+                                            Location: {facility?.location}
+                                        </p>
+                                        <p className="text-primary text-xl mt-2">
+                                            Price per hour: ${facility?.pricePerHour}
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                        <section className="">
                             <h4 className="text-xl font-extrabold text-gray-800">
                                 {facility?.name}
                             </h4>
-                            <p className="text-primary text-xl mt-2">{facility?.description}</p>
                             <p className="text-primary text-xl mt-2">
-                                Location: {facility?.location}
-                            </p>
-                            <p className="text-primary text-xl mt-2">
-                                Price per hour: ${facility?.pricePerHour}
+                                {facility?.description ? (
+                                    <article className="prose lg:prose-xl">
+                                        <div
+                                            className="w-full"
+                                            dangerouslySetInnerHTML={{ __html: facility?.description }}
+                                        />
+                                    </article>
+                                ) : (
+                                    <p>No content available for this blog.</p>
+                                )}
                             </p>
                         </section>
                     </section>
                 </section>
 
-                <div className="flex lg:flex-row gap-8">
-                    {/* User Information */}
-                    <div className="mb-6">
-                        <h3 className="text-2xl font-extrabold text-primary">User</h3>
-                        <div className="mt-4">
-                            <p className="text-primary text-xl">
-                                <strong>Name:</strong> {user?.name}
-                            </p>
-                            <p className="text-primary text-xl">
-                                <strong>Email:</strong> {user?.email}
-                            </p>
-                            <p className="text-primary text-xl">
-                                <strong>Phone:</strong> {user?.phone}
-                            </p>
-                            <p className="text-primary text-xl">
-                                <strong>Address:</strong> {user?.address}
-                            </p>
-                        </div>
-                    </div>
 
-                    {/* Booking Information */}
-                    <div className="mb-6">
-                        <h3 className="text-2xl font-extrabold text-primary">Booking Info</h3>
-                        <div className="mt-4">
-                            <p className="text-primary text-xl">
-                                <strong>Date:</strong> {date ? date : "date"}
-                            </p>
-                            <p className="text-primary text-xl">
-                                <strong>Time:</strong> {startTime ? startTime : "startTime"} - {endTime ? endTime : "endTime"}
-                            </p>
-                            <p className="text-primary text-xl">
-                                <strong>Payable Amount:</strong> ${payableAmount ? payableAmount : "payableAmount"}
-                            </p>
-                            <p className={`text-lg font-semibold mt-4 ${isBooked === "canceled" ? "text-red-500" : "text-green-500"}`}>
-                                <span className="text-primary font-bold"> Status:</span> {isBooked}
-                            </p>
-                        </div>
-                    </div>
-                </div>
             </section>
         </section>
     );
--- src/pages/dashboard/MyBookings.tsx
+++ src/pages/dashboard/MyBookings.tsx
@@ -131,7 +131,7 @@ const MyBookings = () => {
                                 </TableCell>
                             </TableRow>
                         )
-                        })
+                        }
                     </TableBody>
                 </Table>
             </section>
